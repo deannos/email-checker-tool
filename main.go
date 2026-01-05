63bdbc1825b8523cd08002d10eecb585ba9cd0a2
deannos <amjha21122002@gmail.com>
2026-01-06 01:15:18 +0530

    refactor: extract DMARC lookup logic

--- main.go
+++ main.go
@@ -25,12 +25,16 @@ func main() {
 func CheckDomain(domain string) {
 	hasMX := hasMXRecord(domain)
 	hasSPF, spfRecord := getSPFRecord(domain)
+	hasDMARC, dmarcRecord := getDMARCRecord(domain)
 
-	fmt.Printf("%s,%t,%t,%q\n",
+	fmt.Printf(
+		"%s,%t,%t,%q,%t,%q\n",
 		domain,
 		hasMX,
 		hasSPF,
 		spfRecord,
+		hasDMARC,
+		dmarcRecord,
 	)
 }
 
@@ -47,6 +51,20 @@ func getSPFRecord(domain string) (bool, string) {
 
 	for _, record := range txtRecords {
 		if len(record) >= 6 && record[:6] == "v=spf1" {
+			return true, record
+		}
+	}
+	return false, ""
+}
+
+func getDMARCRecord(domain string) (bool, string) {
+	records, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		return false, ""
+	}
+
+	for _, record := range records {
+		if len(record) >= 8 && record[:8] == "v=DMARC1" {
 			return true, record
 		}
 	}
